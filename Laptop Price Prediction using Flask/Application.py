48597053c7807e186b5416f018ef9902af15efec
Sanket-Raone <104383679+Sanket-Raone@users.noreply.github.com>
2022-07-22 17:00:44 +0530

    Add files via upload

---
+++ Laptop Price Prediction using Flask/Application.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template,request,redirect
+from flask_cors import CORS,cross_origin
+import pickle
+import pandas as pd
+import numpy as np
+import os
+
+
+global laptop
+app = Flask(__name__)
+cors = CORS(app)
+model = pickle.load(open('df.pkl','rb'))
+laptop = pd.read_csv('Cleaned_laptop_data.csv')
+
+
+@app.route('/', methods=['GET' , 'POST'])
+def index():
+    global laptop
+    companies = sorted(laptop['Company'].unique())
+    type = sorted(laptop['TypeName'].unique())
+    ram=sorted(laptop['Ram'].unique())
+    screen=laptop['Touchscreen'].unique()
+    ips=laptop['Ips'].unique()
+    ppi=laptop['ppi'].unique()
+    CPU=laptop['Cpu brand'].unique()
+    HDD=laptop['HDD'].unique()
+    SSD=laptop['SSD'].unique()
+    GPU=laptop['Gpu brand'].unique()
+    OS=laptop['os'].unique()
+
+    companies.insert(0,'Select Company')
+    print(companies)
+
+    return render_template('index.html',companies=companies, type=type, ram=ram)
+
+if __name__=='__main__':
+    app.run(debug=True)
